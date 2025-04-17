8125da3b2d4c5ed1772daedbde09ae6962ea415d
adenk <sengsou2@uwm.edu>
2025-04-17 17:00:33 -0500

    Update test file. Allows access to multiple models for testing connection to server/host.

--- ollama_test.py
+++ ollama_test.py
@@ -34,33 +34,3 @@ _ollama_responses("qwen2.5-coder", "Explain the function.")
 
 
 
-# response = requests.post(
-#     "http://localhost:11434/api/generate",
-#     json={
-#         "model": "llama3.2",
-#         "prompt": "Explain the Fibonacci function.",
-#         "stream": True,
-#     },
-#     stream=True
-# )
-# response = requests.post(
-#     url= "http://localhost:11434/api/generate",
-#     json={
-#         "model":"qwen2.5-coder",
-#         "prompt": "Explain the function",
-#         "stream": True,
-#
-#     },
-#     stream=True
-# )
-#
-# print("\nResponse from Ollama:")
-# for line in response.iter_lines():
-#     if line:
-#         try:
-#             data = json.loads(line.decode("utf-8"))
-#             print(data.get("response", ""), end="")  # Print the actual text response
-#             if data.get("done", False):
-#                 break  # Stop when generation is complete
-#         except json.JSONDecodeError:
-#             continue  # Skip malformed lines
